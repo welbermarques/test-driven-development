66b861fc9f44fe506b92edb5f05ca6bc13ee1fcc
Welber Marques Fernandes <welber.fernandes@gdfnet.df.gov.br>
2019-11-28 13:33:51 -0300

    segundo commit

---
+++ src/ConversorDeNumeroRomano.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConversorDeNumeroRomano {
+
+	private static Map<String, Integer> tabela = new HashMap<String, Integer>() {{
+		put("I", 1);
+		put("V", 5);
+		put("X", 10);
+		put("L", 50);
+		put("C", 100);
+		put("D", 500);
+		put("M", 1000);
+	}};
+	
+	public int converte(String numeroEmRomano) {
+		return tabela.get(numeroEmRomano);
+	}	
+}
---
+++ test/ConversorDeNumeroRomanoTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class ConversorDeNumeroRomanoTest {
+
+	@Test
+	public void deveEntenderOSimboloI() {
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("I");
+		assertEquals(1, numero);
+	}
+	
+	@Test
+	public void deveEntenderOSimboloV() {
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("V");
+		assertEquals(5, numero);
+	}
+}
