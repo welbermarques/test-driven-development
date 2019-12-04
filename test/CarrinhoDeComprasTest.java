fb9c15dba65bf644eb88c77d686770cc52785a8b
Welber Marques Fernandes <welber.fernandes@gdfnet.df.gov.br>
2019-12-04 15:12:23 -0300

    Cap. 6 Qualidade no código do teste (update)

--- test/CarrinhoDeComprasTest.java
+++ test/CarrinhoDeComprasTest.java
@@ -8,44 +8,39 @@ import modelo.Item;
 
 public class CarrinhoDeComprasTest {
 
-	private CarrinhoDeCompras carrinho;
+	public CarrinhoDeComprasBuilder carrinho;
 	
 	@Before
 	public void inicializa() {
-		this.carrinho = new CarrinhoDeCompras();
+		carrinho = new CarrinhoDeComprasBuilder();
 	}
 	
 	@Test
 	public void deveAdicionarItens() {
+		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		
 		// garante que o carrinho está vazio
 		assertEquals(0, carrinho.getItens().size());
 		
 		Item item = new Item("Geladeira", 1, 900.0);
 		carrinho.adiciona(item);
-		
 		assertEquals(1, carrinho.getItens().size());
 		assertEquals(item, carrinho.getItens().get(0));
 	}
 	
 	@Test
 	public void deveRetornarZeroSeCarrinhoVazio() {
-		assertEquals(0.0, carrinho.maiorValor(), 0.00001);
+		assertEquals(0.0, carrinho.cria().maiorValor(), 0.00001);
 	}
 	
 	@Test
 	public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
-		carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-		
-		assertEquals(900.0,  carrinho.maiorValor(), 0.00001);
+		assertEquals(900.0,  carrinho.comItens(900.0).cria().maiorValor(), 0.00001);
 	}
 	
 	@Test
 	public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
-		carrinho.adiciona(new Item("Geladeira", 1, 900.0));
-		carrinho.adiciona(new Item("Fogão", 1, 1500.0));
-		carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
-		
-		assertEquals(1500, carrinho.maiorValor(), 0.0001);
+		assertEquals(1500, carrinho.comItens(900.0, 1500.0, 750.0).cria().maiorValor(), 0.0001);
 	}
 	
 }
